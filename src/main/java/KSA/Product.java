52d03c93e0b860c615736591895ae0fe59ad0759
SerzhSeryozhka <Jk3103KS@yandex.ru>
2024-04-24 15:56:56 +0300

    дубль два

--- src/main/java/KSA/Product.java
+++ src/main/java/KSA/Product.java
@@ -1,5 +1,7 @@
 package KSA;
 
+import java.util.Objects;
+
 public class Product {
     int id;
     String name;
@@ -30,5 +32,18 @@ public class Product {
                 ", name='" + name + '\'' +
                 ", cost='" + cost + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && Double.compare(cost, product.cost) == 0 && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, cost);
     }
 }
--- src/main/java/KSA/ProductService.java
+++ src/main/java/KSA/ProductService.java
@@ -8,8 +8,9 @@ import java.util.Scanner;
 
 @Component
 public class ProductService {
-    public Product[] listProduct(){
-        Product[] products = {new Product(1, "Potato", 5),
+    public ArrayList<Product[]> listProduct(){
+        ArrayList<Product[]> products = new ArrayList<>();
+        Product[] product = {new Product(1, "Potato", 5),
                 new Product(2, "Potato", 5),
                 new Product(3, "Помидоры", 15),
                 new Product(4, "Томаты", 25),
@@ -19,12 +20,19 @@ public class ProductService {
                 new Product(8, "Козье молоко", 152),
                 new Product(9, "Не молоко", 304),
                 new Product(10, "Картофель", 3.97)};
+        products.add(product);
         return products;
     }
-//    public poisk(Product[] products){
-//        Scanner scanner=new Scanner(System.in);
-//        String x=scanner.next();
-//        for (int i=0; i<10;i++)
-//            if (products =x
-//    }
+    public void output(){
+        for (Product[] products : listProduct()) {
+            System.out.print(products);
+        }
+
+    }
+    public poisk(Product[] products){
+        Scanner scanner=new Scanner(System.in);
+        String x=scanner.next();
+        for (int i=0; i<10;i++)
+            if (products =x
+    }
 }
